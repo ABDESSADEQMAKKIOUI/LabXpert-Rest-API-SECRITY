15e2ea70df0075e3ed420229de9b8ef0821d0a0b
ABDESSADEQMAKKIOUI <abdessadeq.elmakkioui20@gmail.com>
2024-02-07 22:16:20 +0100

    creat authorization config

--- src/main/java/org/techlab/labxpert/config/SecurityConfig.java
+++ src/main/java/org/techlab/labxpert/config/SecurityConfig.java
@@ -10,8 +10,11 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.techlab.labxpert.Enum.RoleUser;
 import org.techlab.labxpert.security.JWTAuthenticationFilter;
 import org.techlab.labxpert.security.JWTHelper;
+
+import static org.springframework.http.HttpMethod.*;
 
 @Configuration
 @EnableWebSecurity
@@ -25,6 +28,26 @@ public class SecurityConfig {
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http.authorizeRequests()
+                .antMatchers("/api/v1/analyse").hasRole(RoleUser.Responsable.name())
+                .antMatchers("/api/v1/echantillon").hasRole(RoleUser.Responsable.name())
+                .antMatchers("/api/v1/norme").hasRole(RoleUser.Responsable.name())
+                .antMatchers("/api/v1/fournisseur").hasRole(RoleUser.Responsable.name())
+                .antMatchers("/api/v1/numeration").hasRole(RoleUser.Responsable.name())
+                .antMatchers("/api/v1/patient").hasRole(RoleUser.Responsable.name())
+                .antMatchers("/api/v1/outil").hasRole(RoleUser.Responsable.name())
+                .antMatchers("/api/v1/Reactif").hasRole(RoleUser.Responsable.name())
+                .antMatchers("/api/v1/utilisateur").hasRole(RoleUser.Responsable.name())
+                .antMatchers(POST,"/api/v1/Reactif").hasRole(RoleUser.Technicien.name())
+                .antMatchers(POST,"/api/v1/numeration").hasRole(RoleUser.Technicien.name())
+                .antMatchers(POST,"/api/v1/echantillon").hasRole(RoleUser.Technicien.name())
+                .antMatchers(POST,"/api/v1/patient").hasRole(RoleUser.Technicien.name())
+                .antMatchers(GET,"/api/v1/numeration").hasRole(RoleUser.Technicien.name())
+                .antMatchers(PUT,"/api/v1/numeration").hasRole(RoleUser.Technicien.name())
+                .antMatchers(DELETE,"/api/v1/numeration").hasRole(RoleUser.Technicien.name())
+                .antMatchers(POST,"/api/v1/echantillon").hasRole(RoleUser.Preleveur.name())
+                .antMatchers(PUT,"/api/v1/echantillon").hasRole(RoleUser.Preleveur.name())
+                .antMatchers(GET,"/api/v1/echantillon").hasRole(RoleUser.Preleveur.name())
+                .antMatchers(DELETE,"/api/v1/echantillon").hasRole(RoleUser.Preleveur.name())
                 .anyRequest().authenticated();
         http.addFilter(new JWTAuthenticationFilter(authenticationManager(http.getSharedObject(AuthenticationConfiguration.class)), jwtHelper));
 //        http.addFilterBefore(new JWTAuthorizationFilter(jwtHelper), UsernamePasswordAuthenticationFilter.class);
